6544bdb3b54596c1598f204979d812ab33e643ba
JHB <jo7hb@naver.com>
2022-07-22 14:13:45 +0900

    ADD UserAPI Add UserController, UserSerivce, UserRepository, UserDto -jo

---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.ssljjong.ssachedule.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    public Boolean getUser(String email, String pw) {
+        // db연결
+        return true;
+    }
+}
