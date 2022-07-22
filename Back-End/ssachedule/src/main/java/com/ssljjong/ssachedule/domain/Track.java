15500ad6b0d009378e8b6e3ddbeadae1c178f0c6
jayl2sw <jayl2sf@gmail.com>
2022-07-22 17:26:15 +0900

    UPDATE DOMAIN change database structure #lee

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/Track.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/Track.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 @Entity
 @Getter
+@Table(name="user_track")
 public class Track {
 
     @Id @GeneratedValue
     @Column(name = "track_id")
     private Long id;
 
-
     private String name;
     private int gisu;
 
-    @OneToMany(mappedBy = "track", cascade = CascadeType.ALL)
-    private List<UserDomain> users = new ArrayList<>();
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private UserTrack userTrack;
 
     @OneToMany(mappedBy = "track", cascade = CascadeType.ALL)
     private List<Monthlyplan> monthlyplans = new ArrayList<>();
