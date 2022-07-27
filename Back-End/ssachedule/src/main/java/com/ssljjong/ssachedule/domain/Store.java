419cba5a41ca8eeaf99e51f077fe62e6ac31d332
jayl2sw <jayl2sf@gmail.com>
2022-07-27 15:00:00 +0900

    ADD lunchcontroller still only skeleton code need chagnges of Date object #lee

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/domain/Store.java
+++
@@ -1,5 +0,0 @@
-package com.ssljjong.ssachedule.domain;
-
-public enum Store {
-    한식사계,모던키친,동방식객,스냅스낵
-}
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TodoRepository.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TodoRepository.java
@@ -9,6 +9,5 @@ public interface TodoRepository {
     public  void save(Todo todo);
     public  Todo findOne(Long id);
     public List<Todo> findAll();
-
     public List<Todo> findByDue(Date dueDate);
 }
