08b26650666cacc8c25720e591e4048f6966ccae
jayl2sw <jayl2sf@gmail.com>
2022-07-27 10:42:18 +0900

    ADD lunch repository add lunch repository #lee

---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/LunchRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.ssljjong.ssachedule.repository;
+
+import com.ssljjong.ssachedule.domain.Lunch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class LunchRepositoryImpl implements LunchRepository{
+
+    private final EntityManager em;
+
+    @Override
+    public void save(Lunch lunch) {
+        em.persist(lunch);
+    }
+
+    @Override
+    public List<Lunch> findByDate(Date date) {
+        return em.createQuery("select l from Lunch l where l.date = :date").getResultList();
+    }
+}
