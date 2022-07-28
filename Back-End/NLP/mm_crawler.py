840b73f656def8db50e1df70cafc0051f4efa945
jaejun lee <jayl2cu@gmail.com>
2022-07-28 11:33:33 +0900

    UPDATE mm_crawler change method to find channels by user and unlimit number of posts in one page and delete unneccessary posts #lee

--- Back-End/NLP/mm_crawler.py
+++ Back-End/NLP/mm_crawler.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from email import message
 from mattermostdriver import Driver
+from requests import post
+
 
 foo = Driver({
         "url" : "meeting.ssafy.com",
-        "login_id" : "email",
-        "password": "pwd",
+        "login_id" : "username",
+        "password": "password",
         "port" : 443,
     })
 
@@ -13,35 +14,44 @@ foo.login()
 
 print("로그인 성공")
 
+
+# print(foo.users.get_user(user_id='me'))
+
 user_id = foo.users.get_user(user_id='me')['id']
 
 user_teams = foo.teams.get_user_teams(user_id)
 
 messages = []
-
 for user_team in user_teams:
     team_id = user_team['id']
-
-    channels = foo.teams.get_public_channels(team_id)
+    
+    channels = foo.channels.get_channel_for_user(user_id, team_id)
     for channel in channels:
-        try:
-            channel_id = channel['id']
-            dictionary = foo.posts.get_posts_for_channel(channel_id)
-            orders = dictionary['order']
-            for order in orders:
-                messages.append(dictionary['posts'][order]['message'])
+        if "공지" in channel['display_name']:
+            try:
+                channel_id = channel['id']
+                dictionary = foo.posts.get_posts_for_channel(channel_id, {"since":1640962800})
+                for post in dictionary['posts'].values():
+                    if '팀에 가입' not in post['message'] and '채널에 추가' not in post['message'] and '채널에 들어' not in post['message']:
+                        messages.append(post['message'])
+                
+            except:
+                continue
             
-        except:
-            continue
+
+print(messages)
             
 df = pd.DataFrame(messages)
 df['flag'] = 0
-df.to_csv('mattermost_data.txt', sep='§')
+# df.to_csv('data.txt')
+df.to_excel('labeldata.xls')
 
 
 
         
 # import pandas as pd
+
+
 
 # df = pd.read_csv('mattermost_data.txt', sep="§")
 # print(df)
