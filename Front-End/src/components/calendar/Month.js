e700c79c919ea7d794e2a4ae53ffd0492440fcea
전현우 <soshot22@naver.com>
2022-07-30 21:55:56 +0900

    UPDATE FRONTEND add redux #jeon

--- Front-End/src/components/calendar/Month.js
+++ Front-End/src/components/calendar/Month.js
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import styles from "../../../app.module.css";
+import CustomCalendar from "./CustomCalendar";
 
 export default function Month() {
   return (
-    <View style={styles.one}>
-      <Text>Month.js</Text>
+    <View>
+      <View style={[{ flexDirection: "row", margin: 5 }]}>
+        <Button title="싸피" />
+        <Button title="스터디" />
+        <Button title="개인일정" />
+      </View>
+      <CustomCalendar />
     </View>
   );
 }
--- Front-End/src/screens/Calendar.js
+++ Front-End/src/screens/Calendar.js
@@ -1,12 +1,11 @@
-import { View, Text, Button } from "react-native";
+import { View, Button } from "react-native";
 import Month from "../components/calendar/Month";
 import ScheduleList from "../components/calendar/ScheduleList";
 import styles from "../../app.module.css";
 
 export default function Calendar({ navigation }) {
   return (
-    <View style={[styles.center, styles.border]}>
-      <Text>Calendar</Text>
+    <View>
       <Month />
       <ScheduleList navigation={navigation} />
       <Button title="ADD" onPress={() => navigation.navigate("MakeSchedule")} />
