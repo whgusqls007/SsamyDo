ebaf2bfe77eae8f3c4d89823c2c9f353649fbe24
전현우 <soshot22@naver.com>
2022-08-01 17:00:53 +0900

    UPDATE FRONTEND add schedule structure #jeon

--- Front-End/src/screens/Calendar.js
+++ Front-End/src/screens/Calendar.js
@@ -2,13 +2,19 @@ import { View, Button } from "react-native";
 import Month from "../components/calendar/Month";
 import ScheduleList from "../components/calendar/ScheduleList";
 import styles from "../../app.module.css";
+import MakeSchedule from "../components/calendar/MakeSchedule";
 
 export default function Calendar({ navigation }) {
   return (
     <View>
       <Month />
       <ScheduleList navigation={navigation} />
-      <Button title="ADD" onPress={() => navigation.navigate("MakeSchedule")} />
+      <Button
+        title="ADD"
+        onPress={() => {
+          navigation.navigate("MakeSchedule");
+        }}
+      />
     </View>
   );
 }
