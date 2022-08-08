9077e6799abbeeeecf4b3a75327545e61b837366
ms_google <ms_google@naver.com>
2022-08-08 09:00:05 +0900

    FIX Frontend todo & notice #park

--- Front-End/src/components/notice/NoticeList.js
+++ Front-End/src/components/notice/NoticeList.js
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import NoticeItem from "./NoticeItem";
 import styles from "../../../app.module.css";
 
+
+const DATA = [
+  {
+    id: '1',
+    title: '공지1',
+    duedate : '2022-08-03',
+  },
+  {
+    id: '2',
+    title: '공지2',
+    duedate : '2022-08-03'
+  }
+];
+
 export default function NoticeList({ navigation }) {
   return (
-    <View style={styles.one}>
-      <Text>NoticeList.js</Text>
-      <NoticeItem navigation={navigation} />
+    <View style={styles.noticelist}>
+      <ScrollView>
+        {DATA.map((notice)=>(
+          <NoticeItem notice={notice} navigation={navigation} />
+        ))}        
+      </ScrollView>
     </View>
   );
-}
+};
--- Front-End/src/store/store.js
+++ Front-End/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import example from "./example/examSlice";
 import Todo from "./slice/calendar/ToDo";
 import TodoList from "./slice/calendar/TodoList";
+import MainTodo from "./slice/main/MainTodo";
 
 const store = configureStore({
   reducer: {
     example: example.reducer,
     Todo: Todo.reducer,
     TodoList: TodoList.reducer,
+    MainTodo: MainTodo.reducer,
   },
 });
 
