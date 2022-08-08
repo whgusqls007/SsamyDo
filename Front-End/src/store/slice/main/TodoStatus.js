bbdd1ab4588425b782aa72b6b6a6e1a8448239e3
ms_google <ms_google@naver.com>
2022-08-08 17:20:20 +0900

    ADD MainTodo slice maintodo #park

--- Front-End/src/store/slice/main/TodoStatus.js
+++ Front-End/src/store/slice/main/TodoStatus.js
@@ -3,26 +3,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const TodoStatus = createSlice({
   name:'TodoStatus',
+
+  // todo 상태는 로컬에 저장 
   // notice id : , isCompleted : True 
-  initialState: [{}],
+  initialState: [{id: '', isCompleted: false}],
   reducers : {
 
     // saveStatus 현재 상태 저장 
     savestatus: (state) => {
       AsyncStorage.setItem(
-        "MainTodo",
+        "TodoStatus",
         // todo id, iscompleted T 값인 것들만 store에 저장
         JSON.stringify({ TodoId: state.id, isCompleted: true }),
         () => {}
       );
     }, 
-
     // 완료한 투두 생성 
     addstatus: (state, action) => {
       // action.payload로 넣어뒀는데 
       // 완료한 todo id, isCompleted 값 [{},{}] 형식으로 추가 .. 
       state.push(action.payload);
-      console.log(action.payload);
+      // console.log(action.payload);
     },
   }
 })
