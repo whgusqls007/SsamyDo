a70be6b9f42f00673b0da6ac2b1353f9626526db
조현빈 <jo7hb@naver.com>
2022-08-10 10:44:09 +0900

    UPDATE Crawling Module Update crawling module to fix multithreads -jo

--- Back-End/preprocess/preprocess/nlp/views.py
+++ Back-End/preprocess/preprocess/nlp/views.py
@@ -16,14 +16,16 @@ import pytesseract
 import cv2
 import sys
 
-driver = Driver({
-        "url" : "meeting.ssafy.com",
-        "login_id" : "jayl2cu@gmail.com",
+driver = Driver(
+    {
+        "url": "meeting.ssafy.com",
+        "login_id": "jayl2cu@gmail.com",
         "password": "Jaylee15@@",
-        "port" : 443,
-    })
+        "port": 443,
+    }
+)
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 okt = Okt()
 stopwords = [
@@ -49,10 +51,13 @@ stopwords = [
 
 loaded_model = load_model("C:/Users/Jay Lee/preprocess/preprocess/nlp/asset/model.h5")
 
+
 def predict(new_sentence):
-    with open('C:/Users/Jay Lee/preprocess/preprocess/nlp/asset/tokenizer.pickle', 'rb') as handle:
+    with open(
+        "C:/Users/Jay Lee/preprocess/preprocess/nlp/asset/tokenizer.pickle", "rb"
+    ) as handle:
         tokenizer = pickle.load(handle)
-    
+
     new_sentence = re.sub(r"[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "", new_sentence)
     new_sentence = okt.morphs(new_sentence, stem=True)  # 토큰화
     new_sentence = [word for word in new_sentence if not word in stopwords]  # 불용어 제거
@@ -68,7 +73,7 @@ def predict(new_sentence):
 
 
 # Create your views here.
-@api_view(['POST'])
+@api_view(["POST"])
 def preprocess(request):
 
     # MatterMostDriver 로그인
@@ -76,48 +81,46 @@ def preprocess(request):
     print("로그인 성공")
 
     # 파일 있는지 확인
-    text = request.POST.get('text')
-    file_ids = request.POST.get('file_ids')
-    post_id = request.POST.get('post_id')
+    text = request.POST.get("text")
+    file_ids = request.POST.get("file_ids")
+    post_id = request.POST.get("post_id")
     if file_ids:
         file_info = driver.posts.get_file_info_for_post(post_id)
-        
-    if 'image' in file_info[0]['mime_type']:
+
+    if "image" in file_info[0]["mime_type"]:
         data = driver.files.get_file(file_ids).content
 
-
-    encoded_img = np.fromstring(data, dtype = np.uint8)
+    encoded_img = np.fromstring(data, dtype=np.uint8)
     img = cv2.imdecode(encoded_img, cv2.IMREAD_COLOR)
 
-    ocr = pytesseract.image_to_string(img, lang = 'kor+eng', config="")
-    
+    ocr = pytesseract.image_to_string(img, lang="kor+eng", config="")
+
     total_message = text + "\n" + ocr
 
     if predict(total_message):
         print("save")
-        channel_id = request.POST.get('channel_id')
+        channel_id = request.POST.get("channel_id")
         title = total_message.split("\n")[0]
         description = total_message
-        date = request.POST.get('timestamp')
+        date = request.POST.get("timestamp")
 
         notice = {
             "channel_id": channel_id,
             "title": title,
             "file_ids": file_ids,
             "description": description,
-            "date": date
+            "date": date,
         }
-        
+
         serializer = NoticeSerializer(data=notice)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             print("저장완료")
-            return Response(status=status.HTTP_201_CREATED)    
+            return Response(status=status.HTTP_201_CREATED)
 
     print("저장 안됨")
     return Response(status=status.HTTP_200_OK)
 
 
-
 # def preprocess(request):
-    # return
+# return
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/UserController.java
@@ -4,7 +4,6 @@ import com.ssljjong.ssachedule.dto.UserDto;
 import com.ssljjong.ssachedule.entity.Track;
 import com.ssljjong.ssachedule.entity.User;
 import com.ssljjong.ssachedule.repository.TrackRepository;
-import com.ssljjong.ssachedule.service.TrackService;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -28,7 +27,6 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final TrackService trackService;
     private final TrackRepository trackRepository;
 
     @GetMapping("/test")
