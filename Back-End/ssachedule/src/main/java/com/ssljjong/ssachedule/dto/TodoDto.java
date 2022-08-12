953a458c1d60fc0b4a43efb29b2779a86131f999
이재준 <jayl2cu@gmail.com>
2022-08-12 15:23:38 +0900

    UPDATE todo fix todo controller #lee

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/TodoDto.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/TodoDto.java
@@ -2,6 +2,7 @@ package com.ssljjong.ssachedule.dto;
 
 import com.ssljjong.ssachedule.entity.Notice;
 import com.ssljjong.ssachedule.entity.TodoType;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.JoinColumn;
@@ -9,22 +10,30 @@ import javax.persistence.OneToOne;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class TodoDto {
 
-    private Long id;
+    private Integer id;
 
     private Long noticeId;
     private String title;
     private String description;
     private TodoType type;
+    private String startDate;
     private String dueDate;
 
-    public TodoDto(Long id, Long noticeId, String title, String description, TodoType type, String dueDate) {
+    public TodoDto(Integer id) {
+        this.id = id;
+    }
+
+    public TodoDto(Integer id, Long noticeId, String title, String description, TodoType type, String startDate, String dueDate) {
         this.id = id;
         this.noticeId = noticeId;
         this.title = title;
         this.description = description;
         this.type = type;
+        this.startDate = startDate;
         this.dueDate = dueDate;
+
     }
 }
