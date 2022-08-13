4495c40e5933eeadf222fc28ab98aba9ab2d0d5e
ms_google <ms_google@naver.com>
2022-08-13 18:46:53 +0900

    ADD ux-improve notice & schedule textinput, text #park

--- Front-End/App.js
+++ Front-End/App.js
@@ -1,22 +1,39 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
 import MyStack from "./src/router/StackNavigater";
-import styles from "./app.module.css";
+// import styles from "./app.module.css";
 import { Provider } from "react-redux";
 import store from "./src/store/store";
 import axios from "axios";
 import { Provider as PaperProvider } from "react-native-paper";
-
+import { StyleSheet, BackHandler, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useEffect } from "react";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <PaperProvider>
-        <NavigationContainer style={styles.back}>
-          <StatusBar />
-          <MyStack />
-        </NavigationContainer>
-      </PaperProvider>
-    </Provider>
+    <SafeAreaView style={styles.appcontainer}> 
+      <Provider store={store}>
+        <PaperProvider>
+          <NavigationContainer style={styles.back}>
+            <StatusBar />
+            <MyStack />
+          </NavigationContainer>
+        </PaperProvider>
+      </Provider>
+    </SafeAreaView>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  appcontainer : {
+    flex: 1
+  },
+  back:{
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
