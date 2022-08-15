ec261760b4b6c19d2cbe79bb6dbe8e2c0acdec50
ms_google <ms_google@naver.com>
2022-08-16 02:08:39 +0900

    ADD Notice CSS notice, noticelist, noticeitem, noticesearch CSS #park
    Former-commit-id: 7589e313cfa6ac98f1c27a7a6528261668ffae6f

--- Front-End/src/components/notice/NoticeList.js
+++ Front-End/src/components/notice/NoticeList.js
@@ -1,6 +1,6 @@
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from "react-native";
 import NoticeItem from "./NoticeItem";
-import styles from "../../../app.module.css";
+// import styles from "../../../app.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import axios from "axios";
@@ -28,15 +28,15 @@ export default function NoticeList({ navigation, select }) {
   const noticeList = useSelector((state) => state.Notice);
 
   const dispatch = useDispatch();
-  const baseURL = "http://i7e204.p.ssafy.io:8080/api/notice/page/10"
+  const baseURL = "http://i7e204.p.ssafy.io:8080/api/notice/page/1"
   useEffect(() => {
     axios({
       method: "get",
       url: baseURL,
     })
       .then((response) => {
-        // console.log("Notice Axios 요청 성공!");
-        // console.log(response.data);
+        console.log("Notice Axios 요청 성공!");
+        console.log(`notice axios ---------- ${response.data}`);
     
         dispatch({type: "Notice/import", payload: response.data})
         // console.log(getTodo)
@@ -47,12 +47,13 @@ export default function NoticeList({ navigation, select }) {
   }, []);
 
 
+  console.log(`notice list --------------- ${JSON.stringify(noticeList)}`)
 
   // DATA 부분 noticeList로 변경 필요
 
   if (select === 'All') {
     return (
-          <View>
+          <View style={styles.noticelistcontainer}>
             <ScrollView>
               {DATA.map((notice)=>(
                 <NoticeItem key={notice.id} notice={notice} navigation={navigation} />
@@ -63,7 +64,7 @@ export default function NoticeList({ navigation, select }) {
   }
   else if (select === 'MM') {
     return (
-      <View>
+      <View style={styles.noticelistcontainer}>
         <ScrollView>
           {DATA.filter(notice => notice.route === 'MM').map((notice)=>(
             <NoticeItem key={notice.id} notice={notice} navigation={navigation} />
@@ -74,7 +75,7 @@ export default function NoticeList({ navigation, select }) {
   }
   else if (select === 'Edu') {
     return  (
-      <View>
+      <View style={styles.noticelistcontainer}>
         <ScrollView>
           {DATA.filter(notice => notice.route === 'Edu').map((notice)=>(
               <NoticeItem key={notice.id} notice={notice} navigation={navigation} />
@@ -85,3 +86,16 @@ export default function NoticeList({ navigation, select }) {
   }; 
 
 };
+
+const styles = StyleSheet.create({
+  noticelistcontainer: {
+    flexDirection: 'row',    
+    backgroundColor: "#A8D1FF",
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: 10,
+    height: "100%",
+  }
+})
