738fbbbe596ab8c7a3015298f68b34d499f48fa9
33j073 <33j073@gmail.com>
2022-08-16 00:06:44 +0900

    ADD Timeline button CSS Add onPress CSS which changes bgColor of pressed btn #kim

--- Front-End/src/screens/Main.js
+++ Front-End/src/screens/Main.js
@@ -8,6 +8,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getTodo } from "../store/slice/main/MainTodo";
 
 export default function Main({ navigation }) {
+
   const dispatch = useDispatch();
   useEffect(() => {
     // 실제 연결 후 getAllKeys로 통합할 수 있는 지 확인
