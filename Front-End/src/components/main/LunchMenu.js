219de17dd9b013980143ff061c44d31cff8af5ee
33j073 <33j073@gmail.com>
2022-08-16 19:43:40 +0900

    ADD agreement & verification css agreement @ verification page styling 2 #kim

--- Front-End/src/components/main/LunchMenu.js
+++ Front-End/src/components/main/LunchMenu.js
@@ -11,21 +11,19 @@ export default function LunchMenu() {
     return result;
   }
   // axios를 통해서 이번 주 점심 받아오기
-  const baseURL = "http://i7e204.p.ssafy.io:8080/api/plan/weekly/period/";
+  const baseURL = "http://i7e204.p.ssafy.io:8080/api/lunch/date/";
 
-  // axios({
-  //   method: "get",
-  //   url: `${baseURL}${ymdFormat(new Date())}`,
-  // })
-  //   .then((response) => {
-  //     console.log("Weekly Axios 요청 성공!");
-  //     return response.data.data;
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.response);
-  //   });
-
-  //
+  axios({
+    method: "get",
+    url: `${baseURL}${ymdFormat(new Date())}`,
+  })
+    .then((response) => {
+      console.log("Lunch Axios 요청 성공!");
+      return response.data.data;
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
 
   return (
     <View>
