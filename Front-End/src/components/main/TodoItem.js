27e48b2c8bedfb0b7b891d5ce14988974f9ecad8
전현우 <soshot22@naver.com>
2022-08-18 11:23:00 +0900

    ADD todo status filter add todo status filter #jeon

--- Front-End/src/components/main/TodoItem.js
+++ Front-End/src/components/main/TodoItem.js
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 import { get } from "react-native/Libraries/Utilities/PixelRatio";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { todoStatusNow } from "../../store/store";
@@ -12,26 +12,39 @@ import { Linking } from "react-native";
 import id from "faker/lib/locales/id_ID";
 
 // TodoItem에서 보여지는 데이터
-// - 현재 activate 된 설문 / List에서 필터 걸어서 내려옴 
+// - 현재 activate 된 설문 / List에서 필터 걸어서 내려옴
 // - 내가 완료하지 않은 할일 (isCompleted) / item에서 체크한 후 보여주기
-
 
 export default function TodoItem({ navigation, item }) {
   // console.log(item)
   const dispatch = useDispatch();
-  const itemId = item.id
-  const itemTitle = (item.title.includes('건강') ? item.title.slice(4): item.title); 
+  const itemId = item.id;
+  const itemTitle = item.title.includes("건강")
+    ? item.title.slice(4)
+    : item.title;
   // console.log(`item notice ------ ${item.notice}`)
-  
+
   // console.log(typeof(item))
   // const itemDate = item.dueDate.slice(0,8)
 
-  const itemDuedate= (item.dueDate.length === 12 ? item.dueDate.slice(0,8): item.dueDate.slice(0,7));
+  const itemDuedate =
+    item.dueDate.length === 12
+      ? item.dueDate.slice(0, 8)
+      : item.dueDate.slice(0, 7);
 
-  const dDay = (itemDuedate.length === 8 ? itemDuedate - ymdFormat1()  : itemDuedate - ymdFormat2() );
+  const dDay =
+    itemDuedate.length === 8
+      ? itemDuedate - ymdFormat1()
+      : itemDuedate - ymdFormat2();
 
-  const hour = (item.dueDate.length === 12 ? item.dueDate.slice(8,10): item.dueDate.slice(8,9));
-  const min = (item.dueDate.length === 12 ? item.dueDate.slice(10,12): item.dueDate.slice(9,11));
+  const hour =
+    item.dueDate.length === 12
+      ? item.dueDate.slice(8, 10)
+      : item.dueDate.slice(8, 9);
+  const min =
+    item.dueDate.length === 12
+      ? item.dueDate.slice(10, 12)
+      : item.dueDate.slice(9, 11);
   // console.log(dDay)
   // console.log(itemDuedate)
   // console.log(itemDate)
@@ -43,109 +56,159 @@ export default function TodoItem({ navigation, item }) {
   // const hour = item.dueDate.slice(8,10)
   // const min = item.dueDate.slice(10,12)
 
-
   const goEdussafy = useCallback(async () => {
-    const destinationURL = 'https://edu.ssafy.com/edu/board/notice/list.do' 
-    if (await Linking.canOpenURL(destinationURL)) await Linking.openURL(destinationURL)
-  }, [])
+    const destinationURL = "https://edu.ssafy.com/edu/board/notice/list.do";
+    if (await Linking.canOpenURL(destinationURL))
+      await Linking.openURL(destinationURL);
+  }, []);
 
+  const saveCompletedTodo = () => {
+    dispatch({ type: "TodoStatus/addstatus", payload: itemId });
+    dispatch({ type: "TodoStatus/savestatus" });
+  };
 
-  const saveCompletedTodo=()=>{
-    dispatch({type:"TodoStatus/addstatus", payload: itemId })
-    dispatch({type:"TodoStatus/savestatus"})
-  }
+  const deleteCompletedTodo = () => {
+    dispatch({ type: "TodoStatus/deletestatus", payload: itemId });
+    dispatch({ type: "TodoStatus/savestatus" });
+  };
 
   // 오늘날짜 220817
   function ymdFormat1(oriDate = new Date()) {
+    // let utc = oriDate.getTime() + oriDate.getTimezoneOffset() * 60 * 1000;
 
-    let utc = oriDate.getTime() + (oriDate.getTimezoneOffset()*60*1000);
-    
-    let timeDiff = 34*60*60*1000;
-    let kst = new Date(utc + (timeDiff))
+    // let timeDiff = 34 * 60 * 60 * 1000;
+    // let kst = new Date(utc + timeDiff);
 
     let result =
-      kst.getFullYear().toString() +
-      (kst.getMonth() + 1).toString().padStart(2, "0") +
-      kst.getDate().toString().padStart(2, "0");
+      oriDate.getFullYear().toString() +
+      (oriDate.getMonth() + 1).toString().padStart(2, "0") +
+      oriDate.getDate().toString().padStart(2, "0");
     return result;
   }
 
+  // console.log(ymdFormat1());
 
   // 오늘날짜 22817
   function ymdFormat2(oriDate = new Date()) {
-    let utc = oriDate.getTime() + (oriDate.getTimezoneOffset()*60*1000);
-    
-    let timeDiff = 34*60*60*1000;
-    let kst = new Date(utc + (timeDiff))
+    // let utc = oriDate.getTime() + oriDate.getTimezoneOffset() * 60 * 1000;
+
+    // let timeDiff = 34 * 60 * 60 * 1000;
+    // let kst = new Date(utc + timeDiff);
 
     let result =
-      kst.getFullYear().toString() +
-      (kst.getMonth() + 1).toString().padStart(1) +
-      kst.getDate().toString().padStart(2, "0");
+      oriDate.getFullYear().toString() +
+      (oriDate.getMonth() + 1).toString().padStart(1) +
+      oriDate.getDate().toString().padStart(2, "0");
     return result;
   }
 
-
   const todostatus = useSelector((state) => {
-    return state.TodoStatus[0]})
+    return state.TodoStatus[0];
+  });
 
-
-  const nowStatus = (todostatus.includes(itemId))
-
+  const nowStatus = useSelector((state) => {
+    return state.TodoStatus[0].includes(itemId);
+  });
 
   return (
-    <View style={[styles.todoitemcontainer, dDay === 0 && styles.todaycontainer, 
-                  nowStatus && styles.disabled]}>
-        <View style={styles.todoitembox}>
-          <View style={styles.check}>       
-            <TouchableOpacity
-              disabled={nowStatus} 
-              onPress={()=> { if (!nowStatus) saveCompletedTodo()}}>
-              {!nowStatus && <MaterialIcons name="check-box-outline-blank" size={24} color="black" />}
-              {nowStatus && <MaterialIcons name="check-box" size={24} color="black" />}
-            </TouchableOpacity>
-          </View>
+    <View
+      style={[
+        styles.todoitemcontainer,
+        dDay === 0 && styles.todaycontainer,
+        nowStatus && styles.disabled,
+      ]}
+    >
+      <View style={styles.todoitembox}>
+        <View style={styles.check}>
+          <TouchableOpacity
+            onPress={() => {
+              if (!nowStatus) {
+                saveCompletedTodo();
+              } else {
+                deleteCompletedTodo();
+              }
+            }}
+          >
+            {!nowStatus && (
+              <MaterialIcons
+                name="check-box-outline-blank"
+                size={24}
+                color="black"
+              />
+            )}
+            {nowStatus && (
+              <MaterialIcons name="check-box" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.textbox}>
-            <View style={styles.itemtitlebox}>
-              {item.notice === null 
-                ? <TouchableOpacity onPress={goEdussafy}>
-                    <Text numberOfLines={1} ellipsizeMode={"tail"} style={[styles.itemtitle, nowStatus&& styles.disabledtext]}>{itemTitle}</Text>
-                  </TouchableOpacity>
-                : <TouchableOpacity onPress={navigation.navigate("NoticeDetail", { id: item.notice })}>
-                    <Text numberOfLines={1} ellipsizeMode={"tail"} style={[styles.itemtitle, nowStatus&& styles.disabledtext]}>{itemTitle}</Text>
-                  </TouchableOpacity>}
-              {/* <TouchableOpacity onPress={goEdussafy}>
+        <View style={styles.textbox}>
+          <View style={styles.itemtitlebox}>
+            {item.notice === null ? (
+              <TouchableOpacity onPress={goEdussafy}>
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode={"tail"}
+                  style={[styles.itemtitle, nowStatus && styles.disabledtext]}
+                >
+                  {itemTitle}
+                </Text>
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                onPress={navigation.navigate("NoticeDetail", {
+                  id: item.notice,
+                })}
+              >
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode={"tail"}
+                  style={[styles.itemtitle, nowStatus && styles.disabledtext]}
+                >
+                  {itemTitle}
+                </Text>
+              </TouchableOpacity>
+            )}
+            {/* <TouchableOpacity onPress={goEdussafy}>
                 <Text numberOfLines={1} ellipsizeMode={"tail"} style={[styles.itemtitle, nowStatus&& styles.disabledtext]}>{item.title}</Text>
               </TouchableOpacity> */}
-              {/* <Text style={[styles.itemdate, nowStatus&& styles.disabledtext]}>{year}/{month}/{day} {hour}:{min}</Text> */}
-            </View>  
-            
-            <View style={styles.itemdatebox} disabled={nowStatus}>
-              {dDay === 0 
-                ? <Text style={[styles.itemdate, nowStatus&& styles.disabledtext]}>D-DAY</Text> 
-                : <Text style={[styles.itemdate, nowStatus&& styles.disabledtext]}>D-{dDay}</Text>}
-              {dDay === 0 ? <Text style={[styles.itemtime, nowStatus&& styles.disabledtext]}>{hour}:{min}</Text> : null }             
-            </View>
-            
+            {/* <Text style={[styles.itemdate, nowStatus&& styles.disabledtext]}>{year}/{month}/{day} {hour}:{min}</Text> */}
           </View>
 
-          {/* notice id 있는 애들만 상세 정보 보여주게 넘김  */}
-          {/* <TouchableOpacity onPress={() => { item.notice !== null 
+          <View style={styles.itemdatebox} disabled={nowStatus}>
+            {dDay === 0 ? (
+              <Text style={[styles.itemdate, nowStatus && styles.disabledtext]}>
+                D-DAY
+              </Text>
+            ) : (
+              <Text style={[styles.itemdate, nowStatus && styles.disabledtext]}>
+                D-{dDay}
+              </Text>
+            )}
+            {dDay === 0 ? (
+              <Text style={[styles.itemtime, nowStatus && styles.disabledtext]}>
+                {hour}:{min}
+              </Text>
+            ) : null}
+          </View>
+        </View>
+
+        {/* notice id 있는 애들만 상세 정보 보여주게 넘김  */}
+        {/* <TouchableOpacity onPress={() => { item.notice !== null 
                                               ? navigation.navigate("NoticeDetail", { id: item.notice })
                                               : {goEdussafy}}
           }>
             {item.notice !== null && <Text>  🔔</Text>}
             {item.notice === null && <Text>  ⛵</Text>}
           </TouchableOpacity> */}
-        </View>
+      </View>
     </View>
-    );  
-  };
+  );
+}
 
 const styles = StyleSheet.create({
-  todoitemcontainer : {
-    flexDirection: 'row',
+  todoitemcontainer: {
+    flexDirection: "row",
     justifyContent: "flex-start",
     backgroundColor: "#ffffff",
     margin: 10,
@@ -153,9 +216,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
 
-  // 마감일 오늘인 것 배경색 변경 
-  todaycontainer : {
-    flexDirection: 'row',
+  // 마감일 오늘인 것 배경색 변경
+  todaycontainer: {
+    flexDirection: "row",
     // justifyContent: "space-around",
     backgroundColor: "#ffe34f",
     margin: 10,
@@ -164,21 +227,20 @@ const styles = StyleSheet.create({
   },
 
   todoitembox: {
-    flexDirection : "row",
+    flexDirection: "row",
     flexGrow: 1,
     // flexShrink: 1,
-    flexWrap: 'wrap',
-    alignItems: "center"
+    flexWrap: "wrap",
+    alignItems: "center",
   },
 
-
-  itemtitlebox : {
+  itemtitlebox: {
     flexDirection: "row",
-    alignItems : "flex-end",
+    alignItems: "flex-end",
     width: "70%",
     // flexShrink: 1,
     // flexGrow: 1,
-    flexWrap: 'wrap'
+    flexWrap: "wrap",
   },
 
   itemtitle: {
@@ -186,7 +248,6 @@ const styles = StyleSheet.create({
     color: "#111111",
     fontSize: 15,
     // whiteSpace: "pre-wrap",
-
   },
 
   itemdate: {
@@ -202,10 +263,10 @@ const styles = StyleSheet.create({
 
   itemdatebox: {
     flexDirection: "row",
-    alignItems : "flex-end",
+    alignItems: "flex-end",
     // flexShrink: 1,
     // flexGrow: 1,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     marginRight: 20,
   },
 
@@ -215,18 +276,17 @@ const styles = StyleSheet.create({
 
   textbox: {
     flex: 0.9,
-    flexDirection: 'row',
+    flexDirection: "row",
     justifyContent: "space-between",
-    alignItems : "center",
+    alignItems: "center",
   },
 
-  disabled : {
+  disabled: {
     backgroundColor: "#ededed",
-    
   },
 
-  disabledtext : {
+  disabledtext: {
     textDecorationLine: "line-through",
     color: "#888888",
-  }
-})
+  },
+});
